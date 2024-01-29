55e94021d4b236540340bb98364676aba5a1d7a7
David Murray <54763683+thedavidmurray@users.noreply.github.com>
2024-01-29 00:17:47 -0500

    Update RGBMintFactory.test.sol

--- test/RGBMintFactory.test.sol
+++ test/RGBMintFactory.test.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import "ds-test/test.sol";
+import "../contracts/RGBMintFactory.sol";
+
+contract RGBMintFactoryTest is DSTest {
+    RGBMintFactory rgbMintFactory;
+
+    function setUp() public {
+        rgbMintFactory = new RGBMintFactory(/* parameters */);
+    }
+
+    function testMintRGB() public {
+        // Write tests for RGB minting logic
+    }
+}
 
